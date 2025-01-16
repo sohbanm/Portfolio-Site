5f4f0cbf53e10cbfd51886762ba82ee24f0df7ba
Sohban Elahi Malik <99363017+sohbanm@users.noreply.github.com>
2025-01-15 23:25:05 -0500

    minimized

--- src/App.vue
+++ src/App.vue
@@ -1,160 +1,78 @@
-<template> 
-  <v-app id="main"> 
-    
-    <!-- <v-app-bar class="primary navbar" -->
-    <v-app-bar class="navbar"
-      scroll-behaviour="elevate"
-      app
-      fixed
-      dark
-      scroll-target="#scrolling-techniques-5"
-      scroll-threshold="1"
-      height="75"
-    >
-      <v-row>
-        <v-col class="mx-auto">
-          <!-- <h1><b>Portfolio</b></h1> -->
-        </v-col>
-
-        <div v-for="button in items" :key=items.id class="buttons blur">
-          <v-col class="col">
-            <a :href=button.ref :target=button.target >
-              <button class="raise text-white">
-                <v-icon class="v-icon text-white">{{button.icon}}</v-icon>
-                {{button.title}}
-              </button>
-            </a>
-          </v-col>
-        </div>
-
-      </v-row>      
-    </v-app-bar>
-
-
-    <!-- This is where all the components are being called in -->
-    <v-main class="main" style="color:black">
-      <Home id="Home"></Home>
-
-      <Timeline id="Experience"></Timeline>
-
-      <div style="text-align:center;margin: 20vh 0 1vh 0;">
-        <a href="#Projects" class="scroll">Projects <v-icon>mdi-arrow-down-bold</v-icon></a>
-      </div>
-
-      <div id="Projects" style="text-align:center;margin: 25vh 0 0 0;"><p style="opacity:0">L</p> </div>
-
-      <Projects></Projects>
-
-      <div style="text-align:center;margin: 15vh 0 1vh 0;">
-        <a href="#Skills" class="scroll">Skills <v-icon>mdi-arrow-down-bold</v-icon></a>
-      </div>
-
-      <div id="Skills" style="text-align:center;margin: 25vh 0 5vh 0;"><p style="opacity:0">L</p> </div>
-
-      <Skills></Skills>
-
-    </v-main>
-
-    
-  </v-app>
+<template>
+  <div class="portfolio">
+    <h1>Sohban Malik</h1>
+    <p>Software Engineer</p>
+    <p>Class of '26</p>
+    <p>Currently @ <span class="ericsson">Ericsson</span></p>
+    <p>Former @ <span class="rbc">RBC</span>, <span class="pcc">PointClickCare</span>, <span class="verto">Verto Health</span></p>
+    <br>
+    <div>
+      <a href="https://www.linkedin.com/in/sohban" target="_blank">Linkedin</a>
+      <a href="https://drive.google.com/file/d/1fXrt4ylluRuHF0wPB0_kKokwiTNl7RNa/view?usp=drive_link" target="_blank">Resume</a>
+    </div>
+  </div>
 </template>
 
 <script>
-
-import Projects from './components/Projects.vue'; 
-import Home from './components/Home.vue';
-import Skills from './components/Skills.vue';
-import Timeline from './components/Timeline.vue';
-
-
 export default {
-  data: () => ({ 
-    items: [
-      {id: 1, title: 'Home', icon: 'mdi-home-outline', ref: "#" , target: ""},
-      {id: 2, title: 'Experience', icon: "mdi-list-box-outline", ref: "#Experience" , target: ""},
-      {id: 3, title: 'Projects', icon: "mdi-xml", ref: "#Projects" , target: ""},
-      {id: 4, title: 'Skills', icon: "mdi-tools", ref: "#Skills" , target: ""},
-      {id: 5, title: 'Resume', icon: 'mdi-file-account', ref: "https://drive.google.com/file/d/1fXrt4ylluRuHF0wPB0_kKokwiTNl7RNa/view?usp=drive_link", target: "_blank"}
-    ],
-    icons: [
-      {id:1, icon:'mdi-github', link: 'https://github.com/sohbanm'},
-      {id:2, icon: 'mdi-linkedin', link: 'https://www.linkedin.com/in/sohban-e-malik/'},
-    ]
-  }),
-  components: { Projects, Home, Skills, Timeline },
+  name: "Portfolio",
 };
 </script>
 
-<style lang="scss" >
-@import './assets/button.scss';
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Prompt:wght@100;200&display=swap');
 
-html {
-  scroll-behavior: smooth;
+.portfolio {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: rgb(46, 46, 46);
+  color: white;
+  text-align: center;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 600;
 }
 
-.navbar {
-  background-color: rgba(0, 0, 0, 0.5) !important;
+.portfolio h1 {
+  font-size: 3em;
+  margin: 0.5em 0;
 }
 
-a.scroll {
-  font-size: 1.4em;
-  text-decoration: none;
-  border: 0.1em solid black;
-  padding: 0.2em;
-  -webkit-transition: all 500ms ease;
-  -moz-transition: all 500ms ease;
-  -ms-transition: all 500ms ease;
-  -o-transition: all 500ms ease;
-  transition: all 500ms ease;
+.portfolio p {
+  font-size: 1.5em;
+  margin: 0.3em 0;
 }
-a.scroll:hover {
+
+.portfolio a {
+  font-size: 1.5em;
+  margin: 0 1em;
   text-decoration: none;
   color: white;
-  background-color: rgb(95, 95, 95);
-  border: 0.1em solid black;
-  padding: 0.2em;
-} 
-
-.v-icon{
-  margin: 0 0.2em 0 0.2em;
-}
-.text-white{
-  color:white !important;
-}
-</style>
-
-<style scoped>
-button{
-  margin: 0.25em;
-  border: 0.25em;
-  align-content: center;
-}
-a button{
-  font-size: 1.3em;
-  color: rgb(0, 0, 0);
+  border: 2px solid transparent;
+  padding: 0.2em 0.5em;
+  transition: border 0.3s;
 }
 
-.v-icon{
-  color: black;
+.portfolio a:hover {
+  border: 2px solid white;
 }
 
-h1{
-  margin-left:5vh;
-  margin-top: 2vh;
-  font-family: 'Raleway', sans-serif;
+.ericsson, .rbc, .pcc, .verto {
+  transition: color 0.3s;
 }
 
-@media screen and (max-width: 1264px) {
-   .buttons {
-    display: none;
-  }
-  h1{
-    margin-left: 0;
-  }
+.ericsson:hover {
+  color: rgb(0, 26, 255); /* Change this to your desired hover color */
 }
-v-app-bar-nav-icon{
-  margin-top: 250vh;
+.rbc:hover {
+  color: rgb(0, 149, 255); /* Change this to your desired hover color */
+}
+.pcc:hover {
+  color: rgb(123, 255, 67); /* Change this to your desired hover color */
+}
+.verto:hover {
+  color: rgb(0, 191, 255); /* Change this to your desired hover color */
 }
 </style>
